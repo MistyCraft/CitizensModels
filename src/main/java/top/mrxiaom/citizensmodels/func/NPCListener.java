25250999602e0fb07d8995ddda701ec94d3b0a87
人間工作 <coolxiaom95@gmail.com>
2025-05-27 12:07:45 +0800

    apply model to NPC when spawning

---
+++ src/main/java/top/mrxiaom/citizensmodels/func/NPCListener.java
@@ -0,0 +1,102 @@
+package top.mrxiaom.citizensmodels.func;
+
+import com.google.common.collect.Lists;
+import com.ticxo.modelengine.api.ModelEngineAPI;
+import com.ticxo.modelengine.api.model.ActiveModel;
+import com.ticxo.modelengine.api.model.ModeledEntity;
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.event.NPCDamageEvent;
+import net.citizensnpcs.api.event.NPCDeathEvent;
+import net.citizensnpcs.api.event.NPCDespawnEvent;
+import net.citizensnpcs.api.event.NPCSpawnEvent;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import top.mrxiaom.citizensmodels.CitizensModels;
+
+import java.util.List;
+
+public class NPCListener extends AbstractModule implements Listener {
+    public NPCListener(CitizensModels plugin) {
+        super(plugin);
+        registerEvents();
+        for (NPC npc : CitizensAPI.getNPCRegistry().sorted()) {
+            if (npc.isSpawned()) {
+                applyModel(npc);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onNPCSpawn(NPCSpawnEvent e) {
+        applyModel(e.getNPC());
+    }
+
+    @EventHandler
+    public void onNPCDeSpawn(NPCDespawnEvent e){
+        resetModel(e.getNPC());
+    }
+
+    @EventHandler
+    public void onNPCHurt(NPCDamageEvent e) {
+        ModeledEntity modeled = ModelEngineAPI.getModeledEntity(e.getNPC().getEntity());
+        if (modeled != null) {
+            modeled.markHurt();
+        }
+    }
+
+    @EventHandler
+    public void onNPCDead(NPCDeathEvent e) {
+        ModeledEntity modeled = ModelEngineAPI.getModeledEntity(e.getNPC().getEntity());
+        if (modeled != null) {
+            modeled.markRemoved();
+        }
+    }
+
+    public void setNPCModel(NPC npc, String modelId) {
+        if (modelId == null) {
+            npc.data().remove("model-id");
+            resetModel(npc);
+        } else {
+            npc.data().setPersistent("model-id", modelId);
+            applyModel(npc);
+        }
+    }
+
+    public void applyModel(NPC npc) {
+        String modelId = npc.data().get("model-id", null);
+        if (modelId == null) return;
+        ActiveModel model = ModelEngineAPI.createActiveModel(modelId);
+
+        ModeledEntity modeled = ModelEngineAPI.getOrCreateModeledEntity(npc.getEntity());
+        modeled.restore();
+        List<String> keys = Lists.newArrayList(modeled.getModels().keySet());
+        for (String key : keys) {
+            modeled.removeModel(key);
+        }
+        modeled.addModel(model, true);
+        ModelEngineAPI.getAPI().getModelUpdaters().registerModeledEntity(modeled.getBase(), modeled);
+    }
+
+    public void resetModel(NPC npc) {
+        ModeledEntity modeled = ModelEngineAPI.getModeledEntity(npc.getEntity());
+        if (modeled != null) {
+            modeled.restore();
+            List<String> keys = Lists.newArrayList(modeled.getModels().keySet());
+            for (String key : keys) {
+                modeled.removeModel(key);
+            }
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        for (NPC npc : CitizensAPI.getNPCRegistry().sorted()) {
+            resetModel(npc);
+        }
+    }
+
+    public static NPCListener inst() {
+        return instanceOf(NPCListener.class);
+    }
+}
